84078c9bfe841de1511e59f20ebbcaf526f70c73
tsaeBdeRehT <anna.bulavina.02@gmail.com>
2021-06-29 19:17:12 +0300

    ///

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
   <div id="app">
-
+    <Header/>
+    <Reason1
+    v-bind:photos="photos"
+    />
+    <Reason2/>
+    <Reason3 v-bind:comments="comments" @add-comment="AddComment"/>
   </div>
 </template>
 
 <script>
 
+import Reason3 from "@/components/Reason3";
+import Reason2 from "@/components/Reason2";
+import Header from "@/components/Header";
+import Reason1 from "@/components/Reason1";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      photos: [
+        {id: 1, sr: require("./assets/1.jpg"), text: "Новогодняя версия"},
+        {id: 2, sr: require("./assets/2.jpg"), text: "В работе"},
+        {id: 3, sr: require("./assets/3.jpg"), text: "Модная версия"}
+      ],
+      comments: [
+        {id: 1, name: 'Goose', text: 'HUI'},
+        {id: 2, name: 'Goose', text: 'HUI'},
+        {id: 3, name: 'Goose', text: 'HUI'}
+      ]
+    }
+  },
+  methods:{
+    AddComment(newComment) {
+      this.comments.push(newComment);
+      console.log(newComment);
+    }
+  },
   components: {
+    Reason3,
+    Reason2,
+    Reason1,
+    Header
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Lobster&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lobster', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1140px;
+  margin: auto;
+}
+
+body {
+  margin: 0;
+  height: 1000px;
+  background-image: url("./assets/header.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
